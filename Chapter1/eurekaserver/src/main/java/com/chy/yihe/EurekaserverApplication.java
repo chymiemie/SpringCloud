d1d42298880b68ae1cb1a3558fdd548e7d91e4fe
chengyang <chengyang@yihecloud.com>
2017-09-20 18:05:13 +0800

    服务的注册与发现

---
+++ Chapter1/eurekaserver/src/main/java/com/chy/yihe/EurekaserverApplication.java
@@ -0,0 +1,19 @@
+package com.chy.yihe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaserverApplication 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(EurekaserverApplication.class, args);
+    }
+}
