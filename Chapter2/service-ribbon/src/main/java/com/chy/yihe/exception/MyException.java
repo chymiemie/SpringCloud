e7b62f242e2c6023339764679996fcfe2e8df0ca
chengyang <chengyang@yihecloud.com>
2017-09-20 18:10:54 +0800

    服务消费者(服务之间相互调用)

---
+++ Chapter2/service-ribbon/src/main/java/com/chy/yihe/exception/MyException.java
@@ -0,0 +1,17 @@
+package com.chy.yihe.exception;
+
+public class MyException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public MyException(String msg) {
+		super(msg);
+		// TODO Auto-generated constructor stub
+	}
+
+    
+
+}
