767eb5020f40515a7e17279a54d90c069d5d3fc1
chengyang <chengyang@yihecloud.com>
2017-09-20 18:13:31 +0800

    断路器（Hystrix）

---
+++ Chapter3/serice-feign/src/main/java/com/chy/yihe/service/SchedualServiceHi.java
@@ -0,0 +1,13 @@
+package com.chy.yihe.service;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "eurekaclient", fallback = SchedualServiceHiHystric.class)
+public interface SchedualServiceHi {
+	@RequestMapping(value = "/hi", method = RequestMethod.GET)
+	public String sayHiFromClientOne(@RequestParam(value = "name") String name);
+
+}
